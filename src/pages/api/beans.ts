395cbe586d318254bdc73dd1abccaf8726a9f091
cjstock <coreystock@gmail.com>
2022-07-11 18:29:30 -0700

    tested working DB and API

---
+++ src/pages/api/beans.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+
+const beans = async (req: NextApiRequest, res: NextApiResponse) => {
+  const beans = await prisma.example.findMany();
+  res.status(200).json(beans);
+};
+
+export default beans;

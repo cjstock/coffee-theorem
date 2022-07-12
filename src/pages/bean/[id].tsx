102ca82533d5938d85ceab598cd4ae093a83cafa
cjstock <coreystock@gmail.com>
2022-07-12 15:42:10 -0700

    Added Layout component

--- src/pages/bean/[id].tsx
+++ src/pages/bean/[id].tsx
@@ -1,16 +1,19 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import Layout from "../../components/layout";
 import { trpc } from "../../utils/trpc";
 
 const BeanPage: NextPage = () => {
     const id = useRouter().query.id as string;
-    const {data, isLoading, isError} = trpc.useQuery(["beans.byId", {id}])
+    const { data, isLoading, isError } = trpc.useQuery(["beans.byId", { id }])
 
     if (isLoading || !data) return <div>Loading...</div>
     if (isError) return <div>ERROR!</div>
 
     return (<>
-        <div>{data.country}</div>
+        <Layout>
+            <div>{data.country}</div>
+        </Layout>
     </>)
 }
 
--- src/pages/mybeans.tsx
+++ src/pages/mybeans.tsx
@@ -6,6 +6,7 @@ import BeanCard from "../components/beancard"
 const MyBeans: NextPage = () => {
     const { data, isLoading, isError } = trpc.useQuery(["beans.getAll"])
 
+
     return (<>
         <div className="mx-auto px-4 lg:max-w-5xl md: max-w-2xl">
             <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 place-content-center">

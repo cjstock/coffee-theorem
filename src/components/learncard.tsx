a76b51ff88745304e9c48ed1f47785e76984b241
Corey Stock <coreystock@gmail.com>
2022-08-08 13:22:12 -0700

    Fixed mobile layout

--- src/components/learncard.tsx
+++ src/components/learncard.tsx
@@ -8,8 +8,8 @@ interface LearnCardProps {
 
 const LearnCard = (props: LearnCardProps) => {
     return (
-        <div className="shadow-xl card w-80 h-80 bg-base-100 image-full">
-            <figure><Image src={props.image} layout="fill"/></figure>
+        <div className="shadow-xl card w-96 h-64 md:w-80 md:h-80 bg-base-100 image-full">
+            <figure className="relative"><Image src={props.image} layout="fill"/></figure>
             <div className="card-body">
                 <h2 className="card-title text-2xl">{props.title}</h2>
                 <p className="italic">{props.description}</p>

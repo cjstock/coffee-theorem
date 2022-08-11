063912fb67bf3949dda00f73e28021392e8bf672
Corey Stock <coreystock@gmail.com>
2022-08-11 13:34:56 -0700

    Added some info

--- src/pages/learn/about-tree.tsx
+++ src/pages/learn/about-tree.tsx
@@ -5,8 +5,9 @@ const AboutTreePage: NextPage = () => {
 
     return (
         <Layout selectedBottomTab="learn">
-            <h1 className="text-4xl place-self-start font-bold">The Coffee Tree</h1>
-            <p>&emsp;This section deals only with the most interesting of the coffee specier, Coffea arabica. At first glance, all Arabica trees look similar: a thin trunk with numerous brances coming off it, supporting foliage and fruit. However, if you look closer there are many differences bewteen trees, determined by the varitey of Arabica being grown. Diefferent varieties yield different amounts of fruit, in different colors, and some carry the fruit in clusters, while others have fruit evernly spaced down the branch.</p>
+            <h1 className="text-4xl place-self-start font-bold p-4">The Coffee Tree</h1>
+            <p>&emsp;While there are two species of coffee plants, <em>Coffea Arabica</em> and <em>Coffea Canephora</em> (commonly refered to as Robusta), we will focus exclusively on the former. Although plenty of farms produce Robusta beans, they are not as tasty to brew and drink. Robusta beans are appealing to producers because of their more forgiving and consistent growing attributes, including: lower growing altitudes, higher growing temperatures, and higher resistance to disease. These factors lead to a cheaper and more resiliant bean which is lower quality than Arabica. This doesn't stop large manufacturing plants from spitting out what calculates to 40% of the world's coffee production each year.</p>
+            <p>&emsp;</p>
         </Layout>
     )
 }

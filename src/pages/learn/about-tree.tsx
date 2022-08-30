e3bb01a6132f7a658d7ea1bfe4f2cf980a4a0510
Corey Stock <coreystock@gmail.com>
2022-08-29 18:33:12 -0700

    Changed text/image alignment

--- src/pages/learn/about-tree.tsx
+++ src/pages/learn/about-tree.tsx
@@ -10,31 +10,27 @@ const AboutTreePage: NextPage = () => {
             <h1 className="text-4xl place-self-start font-bold p-4">
                 The Coffee Tree
             </h1>
-            <div className="md:columns-2">
-                <p>
-                    There are over 120 known coffee tree species in existence.
-                    However, only 2 are grown and cultivated in any meaningful
-                    quantity; <em>Coffea Arabica</em> and{" "}
-                    <em>Coffea Canephora</em>
-                    (commonly referred to as Robusta). Further secitons of this
-                    app will focus exclusively on Arabica beans, due to their
-                    higher prevalence in modern specialty coffee culture.
-                    Robusta plants can grow at lower altitudes, higher
-                    temperatures, and are generally more resistant to diseases.
-                    Unfortunately, they tend to lack in flavor and complexity
-                    compared to Arabica. Robusta plants produce coffee that
-                    tends to taste woody, with a lacking acidity and a heavy
-                    body. Although Robusta has been a staple of Italian espresso
-                    culture for decades, most is grown for use as... instant
-                    coffee!
-                </p>
-                <figure>
-                    <Image src={arabicaDiagram} />
-                    <figcaption>
+            <p className="text-left">
+                <span className="w-96 float-right m-5">
+                    <Image src={arabicaDiagram} layout="responsive" />
+                    <span>
                         <em>Coffea Arabica diagram</em>
-                    </figcaption>
-                </figure>
-            </div>
+                    </span>
+                </span>
+                There are over 120 known coffee tree species in existence.
+                However, only 2 are grown and cultivated in any meaningful
+                quantity; <em>Coffea Arabica</em> and <em>Coffea Canephora</em>
+                (commonly referred to as Robusta). Further secitons of this app
+                will focus exclusively on Arabica beans, due to their higher
+                prevalence in modern specialty coffee culture. Robusta plants
+                can grow at lower altitudes, higher temperatures, and are
+                generally more resistant to diseases. Unfortunately, they tend
+                to lack in flavor and complexity compared to Arabica. Robusta
+                plants produce coffee that tends to taste woody, with a lacking
+                acidity and a heavy body. Although Robusta has been a staple of
+                Italian espresso culture for decades, most is grown for use
+                as... instant coffee!
+            </p>
         </Layout>
     );
 };

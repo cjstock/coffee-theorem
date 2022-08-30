17dd61346d2046137343d6256e8333c4f29a70cf
Corey Stock <coreystock@gmail.com>
2022-08-29 18:02:32 -0700

    Fixed build error

--- src/pages/learn/about-fruit.tsx
+++ src/pages/learn/about-fruit.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import Layout from "../../components/layout";
 
 const AboutFruitPage: NextPage = () => {
-  return <Layout selectedBottomTab="learn"></Layout>;
+    return <Layout selectedBottomTab="learn">Fruit</Layout>;
 };
 
 export default AboutFruitPage;

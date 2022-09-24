6608adff8930c7cfba9f15375e13725edfe4a22a
Corey Stock <coreystock@gmail.com>
2022-09-24 13:33:17 -0700

    removed example trpc route

--- src/server/trpc/router/index.ts
+++ src/server/trpc/router/index.ts
@@ -1,11 +1,9 @@
 // src/server/trpc/router/index.ts
 import { t } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
 import { coffeeRouter } from "./coffee";
 
 export const appRouter = t.router({
-  example: exampleRouter,
   auth: authRouter,
   coffee: coffeeRouter,
 });

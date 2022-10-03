e6ad34e04985ec8dc75769aa3120969e81b69ea6
Corey Stock <coreystock@gmail.com>
2022-10-02 23:21:07 -0700

    Hopefully fixed deploy errors

--- src/components/pages/coffee-collection/BeanGrid.tsx
+++ src/components/pages/coffee-collection/BeanGrid.tsx
@@ -1,10 +1,10 @@
-import { Bean } from "@prisma/client";
+import { Coffee } from "@prisma/client";
 import { Reorder } from "framer-motion";
 import { useState } from "react";
 import BeanCard from "./BeanCard";
 
 interface Props {
-    beans: Array<Bean>;
+    beans: Array<Coffee>;
 }
 const BeanGrid: React.FC<Props> = ({ beans }: Props) => {
     const [beanState, setBeanState] = useState(beans);
@@ -20,8 +20,8 @@ const BeanGrid: React.FC<Props> = ({ beans }: Props) => {
             drag={false}
             className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
         >
-            {beanState.map((bean) => (
-                <BeanCard key={bean.id} bean={bean} />
+            {beanState.map((coffee) => (
+                <BeanCard key={coffee.id} coffee={coffee} />
             ))}
         </Reorder.Group>
     );
--- src/server/trpc/router/tastingNotes.ts
+++ src/server/trpc/router/tastingNotes.ts
@@ -6,28 +6,4 @@ export const tastingNotesRouter = t.router({
         .query(({ ctx }) => {
             return ctx.prisma.tastingNote.findMany({});
         }),
-    add: t.procedure
-        .input(z.object({ coffeeId: z.string(), note: z.string(), id: z.string() }))
-        .mutation(({ ctx, input }) => {
-            return ctx.prisma.tastingNote.upsert({
-                where: {
-                    id: input.id
-                },
-                create: {
-                    note: input.note,
-                    coffees: {
-                        connect: {
-                            id: input.coffeeId
-                        }
-                    }
-                },
-                update: {
-                    coffees: {
-                        connect: {
-                            id: input.coffeeId
-                        }
-                    }
-                }
-            })
-        })
 });

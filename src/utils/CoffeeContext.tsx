0fe46a5f335f9ab27a43d3dbe9ede11f471491f2
Corey Stock <coreystock@gmail.com>
2022-10-05 15:56:56 -0700

    Created Coffee Context

---
+++ src/utils/CoffeeContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, useContext, useReducer, ReactNode, Dispatch } from 'react';
+import { initialState, ACTIONTYPE, reducer } from './CoffeeReducer';
+
+const CoffeeContext = createContext(initialState);
+
+const CoffeeDispatchContext = createContext<Dispatch<ACTIONTYPE>>({} as Dispatch<ACTIONTYPE>);
+
+interface Props {
+    children: ReactNode;
+}
+export function CoffeeProvider({ children }: Props) {
+    const [coffee, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <CoffeeContext.Provider value={coffee}>
+            <CoffeeDispatchContext.Provider value={dispatch}>
+                {children}
+            </CoffeeDispatchContext.Provider>
+        </CoffeeContext.Provider>
+    )
+}
+
+export function useCoffee() {
+    return useContext(CoffeeContext)
+}
+
+export function useCoffeeDispatch() {
+    return useContext(CoffeeDispatchContext)
+}

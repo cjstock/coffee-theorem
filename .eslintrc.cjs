70972e351ef3e4502c2ee9c9af90a0a018d26fcc
Corey Stock <coreystock@gmail.com>
2023-07-27 19:48:50 -0700

    Installed Storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,18 @@
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: true
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended-type-checked", "plugin:@typescript-eslint/stylistic-type-checked", "plugin:storybook/recommended"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
+    "@typescript-eslint/consistent-type-imports": ["warn", {
+      prefer: "type-imports",
+      fixStyle: "inline-type-imports"
+    }],
+    "@typescript-eslint/no-unused-vars": ["warn", {
+      argsIgnorePattern: "^_"
+    }]
+  }
 };
-
 module.exports = config;
